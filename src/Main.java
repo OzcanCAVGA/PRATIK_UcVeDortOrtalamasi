1cc161ace094de4f41f678f5a834398c12ec5a3c
Özcan ÇAVGA <81030695+OzcanCAVGA@users.noreply.github.com>
2022-09-29 20:01:44 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
+/*
+Java döngüler ile 0'dan girilen sayıya kadar olan sayılardan
+3 ve 4'e tam bölünen sayıların ortalamasını hesaplayan programı yazınız.
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int k, count = 0, toplam = 0,yeniSayi;
+        Scanner yazici = new Scanner(System.in);
+        System.out.println("Girilecek sayi adedini girin");
+        k = yazici.nextInt();
+        for (int i = 1; i <= k; i++) {
+            yeniSayi=yazici.nextInt();
+            if (yeniSayi % 3 == 0 && yeniSayi % 4 == 0) {
+                toplam = yeniSayi + toplam;
+                count++;
+            }
+        }
+        System.out.println("ortalama: "+(toplam/count));
     }
 }
